b2f30b9a1104be9b25d623082f0cb1b268ed3154
Jose <josep.ibanez@gft.com>
2016-07-07 11:45:15 +0200

    Solve a problem with don't charge the lists. Solve the problem with don't appear a list of purchased, products. Now Appears with number Id.

--- app/components/cart/cart.Controller.js
+++ app/components/cart/cart.Controller.js
@@ -5,16 +5,17 @@ angular.module('App.Controllers')
 .controller('cartController',
     function ($log, $scope, $http, listService) {
         $log.debug('cartController loading');
-
-        $scope.greeting = 'Welcome';
+        var self = this;
+        self.greeting = 'Welcome';
 
         $http.get('components/stock/stocks.json').then(function(data){
         	$scope.stocks = data.data;
         	console.log('data llegida', $scope.stocks);
     	});
         $scope.getCart = function(){
-            var list = listService.getList();
-            console.log(list);
+            self.list = listService.getList();
+            console.log(self.list);
         };
+        $scope.getCart();
 	}
 );
