c15a5ca0aee5acf5358e265f0d7ecedd158c1508
Jose <josep.ibanez@gft.com>
2016-07-11 12:23:29 +0200

    Solve the problem with total price, works fine, and reorganize the page for show the list always, and appears the purchase and remove list only have products.

--- app/components/cart/cart.Controller.js
+++ app/components/cart/cart.Controller.js
@@ -8,24 +8,25 @@ angular.module('App.Controllers')
         var self = this;
         $scope.empty= false;
         self.greeting = 'Welcome';
-
+        self.totalPrice=0;
         $scope.getCart = function(){
             self.list = listService.getList();
             console.log('Lista Cargada');
         };
-
         $scope.getCart();
-
         if(self.list >= 0){
             $scope.empty= true;
         } else{
             $scope.empty= false;
         };
-
+        for(self.i=0; self.i<self.list.length; self.i++){
+            self.totalPrice= self.totalPrice + self.list[self.i].price;
+        };
+        
         $scope.removePurchase = function(id){
             console.log(':D Prueba!')
         };
-
+        
         $scope.removeCart = function(){
             self.list = listService.removeList();
             if(self.list >= 0){
@@ -33,8 +34,10 @@ angular.module('App.Controllers')
             } else{
                 $scope.empty= false;
             };
+            self.totalPrice=0;
             console.log('Lista Borrada');
         };
+        
         $scope.confirmCart = function () {
             return confirm('Are you sure you want to delete cart ?');
         };
